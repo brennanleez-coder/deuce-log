fce2e9d4c23202001552986fd3956395c085a960
brennanleez-coder <brennanlee95@gmail.com>
2025-02-11 03:25:23 +0800

    website metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'DeuceLog',
+  description: 'A simple log',
 }
 
 export default function RootLayout({
